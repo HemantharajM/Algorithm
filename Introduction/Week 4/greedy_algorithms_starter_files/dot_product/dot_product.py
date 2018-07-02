8c27fbaf8c1093aa33dc2653d8bd02ee486ff2fe
HemantharajM <iro2017001@iiita.ac.in>
2018-07-02 16:11:03 +0530

    added fractional knapsack problem

--- Introduction/Week 4/greedy_algorithms_starter_files/dot_product/dot_product.py
+++ Introduction/Week 4/greedy_algorithms_starter_files/dot_product/dot_product.py
@@ -5,15 +5,19 @@ import sys
 def max_dot_product(a, b):
     #write your code here
     res = 0
+    a.sort()
+    b.sort()
     for i in range(len(a)):
         res += a[i] * b[i]
     return res
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    a = data[1:(n + 1)]
-    b = data[(n + 1):]
+    token = input().split()
+    n = int(token[0])
+    a = input().split()
+    a = [int(i) for i in a]
+    b = input().split()
+    b = [int(i) for i in b]
+
     print(max_dot_product(a, b))
     
--- Introduction/Week 4/greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.py
+++ Introduction/Week 4/greedy_algorithms_starter_files/fractional_knapsack/fractional_knapsack.py
@@ -4,14 +4,32 @@ import sys
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
+    n = len(weights)
+    fraction = [values[i] / weights[i] for i in range(n)]
+    
+    indices = sorted(range(len(fraction)), key = fraction.__getitem__, reverse = True)
 
+    for i in indices:
+        if capacity > weights[i]:
+            value = value + values[i]
+            capacity = capacity - weights[i]
+        else :
+            value = value + (capacity * fraction[i])
+            capacity = capacity - capacity
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
+    token = input().split()
+    n = int(token[0])
+    capacity = int(token[1])
+    values = [0 for i in range(n)] 
+    weights = [0 for i in range(n)]
+
+    for i in range(n):
+        token = input().split()
+        values[i] = int(token[0])
+        weights[i] = int(token[1])
+
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
